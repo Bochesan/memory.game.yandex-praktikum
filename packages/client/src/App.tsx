e2ba4ba5b90ee3285298838289c2ccf394169310
sanityFair <106737730+sanityFair@users.noreply.github.com>
2024-05-20 21:01:53 +0300

    feat(client): add error-boundary PSS-26

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,6 @@
-import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
   return <div className="App">Вот тут будет жить ваше приложение :)</div>
 }
 
