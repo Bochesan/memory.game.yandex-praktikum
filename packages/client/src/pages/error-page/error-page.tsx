63f0af8e632487fce030eda087cb9be5708d1c30
sanityFair <106737730+sanityFair@users.noreply.github.com>
2024-05-23 16:53:04 +0300

    feat(client): add error page

--- packages/client/src/pages/error-page/error-page.tsx
+++ packages/client/src/pages/error-page/error-page.tsx
@@ -1,3 +1,12 @@
-export const ErrorPage = () => {
-  return <div>error-page</div>
-}
+import { Button } from '@mui/material'
+import svgUrl from '@/assets/btn-menu.png'
+
+import styles from './styles.module.css'
+
+export const ErrorPage = () => (
+  <div className={styles.bg}>
+    <Button href="/">
+      <img src={svgUrl} alt="btn-menu" />
+    </Button>
+  </div>
+)
