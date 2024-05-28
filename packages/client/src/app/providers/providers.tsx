db9001d7ab94d742d963ba33f2be4f91aaff7725
sanityFair <106737730+sanityFair@users.noreply.github.com>
2024-05-28 13:42:35 +0300

    feat(client): add levels page

--- packages/client/src/app/providers/providers.tsx
+++ packages/client/src/app/providers/providers.tsx
@@ -1,13 +1,16 @@
 import { ErrorBoundary } from '@/shared/components'
 import { Provider } from 'react-redux'
 import { store } from '@/app/store'
-import { ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { theme } from '@/app/styles'
 
 export const Providers = ({ children }: { children: JSX.Element }) => (
   <ErrorBoundary fallback={<>Something wrong</>}>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
     </Provider>
   </ErrorBoundary>
 )
---
+++ packages/client/src/shared/components/level-map/item/item.tsx
@@ -0,0 +1,54 @@
+import { Filter } from './filter'
+import classNames from 'classnames'
+
+import styles from './styles.module.css'
+
+type Props = {
+  level?: number
+  isPassed?: boolean
+  isCurrent?: boolean
+  x?: number
+  y?: number
+}
+
+export const Item = ({ x, y, level, isCurrent, isPassed }: Props) => (
+  <svg
+    width={200}
+    height={200}
+    viewBox="0 0 200 200"
+    className={styles.root}
+    x={x}
+    y={y}>
+    <g className={styles.wrapper}>
+      <clipPath id="cut-off" className={styles.clipPath}>
+        <rect x="110" y="110" width="95" height="95" />
+        <rect x="110" y="110" width="95" height="95" />
+      </clipPath>
+      <rect
+        x="115"
+        y="115"
+        width="85"
+        height="85"
+        stroke="#01cbfd"
+        clipPath="url(#cut-off)"
+      />
+      <rect x="120" y="120" stroke="#01cbfd" width="75" height="75" />
+      <rect
+        x="130"
+        y="130"
+        width="55"
+        height="55"
+        fill="#010302"
+        className={classNames('', {
+          [styles.current]: isCurrent,
+          [styles.passed]: isPassed,
+        })}
+        filter="url(#shadow)"
+      />
+    </g>
+    <Filter />
+    <text className={styles.text} x="66%" y="73%">
+      <tspan>{level}</tspan>
+    </text>
+  </svg>
+)
