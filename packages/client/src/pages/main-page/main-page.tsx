d1f39b25e2e816e3fa200c73beda18de633195db
sanityFair <106737730+sanityFair@users.noreply.github.com>
2024-06-01 22:44:17 +0300

    feat(client): add main page

--- packages/client/src/pages/main-page/main-page.tsx
+++ packages/client/src/pages/main-page/main-page.tsx
@@ -1,9 +1,19 @@
-import { Typography } from '@mui/material'
+import { Navigation, UserInfo, Experience } from '@/shared'
+import bgUrl from '@/assets/bg.png'
+
+import styles from './styles.module.css'
 
 export const MainPage = () => {
   return (
-    <div>
-      <Typography variant="h1">Main Page</Typography>
+    <div className={styles.root}>
+      <img src={bgUrl} className={styles.bg} />
+      <div className={styles.menu}>
+        <Navigation />
+      </div>
+      <div className={styles.info}>
+        <Experience level={0} />
+        <UserInfo />
+      </div>
     </div>
   )
 }
