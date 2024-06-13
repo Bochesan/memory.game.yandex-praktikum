6da2c861e5595f1face137b9270877957ec8f335
Pavel Zabolotny <p.zabolotny@bi.zone>
2024-06-13 23:45:09 +0300

    PSS-52 | Web Api Fullscreen

---
+++ packages/client/src/shared/components/fullscreen/fullscreen.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.css'
+import React, { useState, useCallback, useEffect, useRef } from 'react'
+import { ICONS } from '@/shared/constants/icons'
+
+export const Fullscreen: React.FC = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false)
+  const documentElement = useRef(document.documentElement)
+  const handleFullscreenToggle = useCallback(() => {
+    setIsFullscreen(prevIsFullscreen => !prevIsFullscreen)
+  }, [])
+
+  useEffect(() => {
+    if (isFullscreen) {
+      documentElement.current.requestFullscreen()
+    } else if (document.fullscreenElement) {
+      document.exitFullscreen()
+    }
+  }, [isFullscreen])
+
+  return (
+    <div className={styles.fullscreen} onClick={handleFullscreenToggle}>
+      <img
+        className={`${styles.fullscreen__icon} ${
+          isFullscreen ? styles.fullscreen__icon_active : ''
+        }`}
+        src={isFullscreen ? ICONS.FullscreenIn : ICONS.FullscreenOut}
+        alt="Fullscreen"
+      />
+    </div>
+  )
+}
